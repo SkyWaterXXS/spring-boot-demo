fd821ce87a2e0f506d99ffd008de6790e9512411
‘xiaoshuo.xu’ <xiaoshuo.xu@wormpex.com>
2018-01-16 15:59:25 +0800

    init

---
+++ src/main/java/com/example/springbootdemo/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.example.springbootdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author xuxiaoshuo 2018/1/16
+ */
+@Controller
+@RequestMapping("/helloworld")
+public class HelloWorldController {
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "hello world";
+    }
+}
